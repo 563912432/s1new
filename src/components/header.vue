2c5715bac74da4e90bb3f2c467f3b78c4d5c17ed
陈帅 <1301491593@qq.com>
2018-07-23 15:07:34 +0800

    'firstcommit'

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="vheader">
+    <img src="../assets/header.png" alt="">
+  </div>
+</template>
+<script>
+  export default {
+    name: 'vheader'
+  }
+</script>
+<style scoped>
+  .vheader{
+    height: 75px;
+    width: 100%;
+    padding: 0;
+    z-index: 1;
+  }
+  .vheader img{
+    height: 100%;
+    width: 100%;
+  }
+</style>
